301aadc3712c5d9323640e6a8ec0cc017bb86813
Evan Ordona <evanordona@hotmail.com>
2022-12-06 02:08:24 -0500

    started navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 
 function App() {
   return (
-    <div className="App">
-      <div>Hello World! Testing</div>
+    <div>
+       <Navbar />
+        
     </div>
   );
 }
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import Navbar from './components/Navbar.js'
+
+
+const Navbar = () => {
+  const list = [
+    {
+        id: 1,
+        title: 'Home',
+    },
+    {
+        id: 2,
+        title: 'Github',
+    },
+
+  ];
+
+
+  return (
+    <div>
+       <div>
+            NBA Odds Calculator
+       </div>
+
+       <div>
+           
+       </div>
+
+
+
+    </div>
+  )
+}
+
+export default Navbar
